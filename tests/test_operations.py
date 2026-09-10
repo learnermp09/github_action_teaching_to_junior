72e7e1f8e4410688c011ff1654ef5dca2afd0b9e
Mrityunjay Pathak <learnermp09@gmail.com>
2026-09-10 17:53:54 +0530

    source and test files are ready

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.operations import *
+
+def test_add():
+    assert add(5, 10) == 15
+    assert add(3, 18) == 21
+
+def test_subs():
+    assert subs(5, 10) == -5
+    assert subs(10, 2) == 8
+
+def test_mult():
+    assert mult(5, 10) == 50
+    assert mult(2, 7) == 14
